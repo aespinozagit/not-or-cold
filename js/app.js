3ce85e3f47a42d82bc61875bac3749d9f938a601
Alex Espinoza <aespinoza123@gmail.com>
2015-07-11 08:08:13 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+
+
+$(document).ready(function(){
+
+	var  secretNumber, userimput, guessCounter = 0;
+
+	
+	/*--- Display information modal box ---*/
+    $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+     });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000); 
+    });
+
+/*--- Reload page on click---*/
+    $(".new").click(function(){
+    location.reload();
+    });
+
+// Generate secret number
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    secretNumber =  getRandomInt(1, 100);
+
+// Get the user input, clear the form, and update guessCounter on submit of userInput
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        userInput = $('#userGuess').val();
+        $(this)[0].reset();
+        guessCounter++;
+        $('#count').html((+guessCounter));
+        $('ul#guessList').append("<li>" + userInput + "<li");
+
+// verify the user input ; number betwee 1-100
+//compare input to secret number and output 'hot' or 'cold' to div#feedback - default setting is 'make your guess!' text 
+    function guessDifference() {
+        return (Math.abs(userInput - secretNumber)); 
+    }
+    function insertFeedback (text) {
+        $('h2#feedback').replaceWith("<h2 id=\"feedback\">" + text +"</h2>"); 
+    }
+    if (isNaN(userInput)) {
+        insertFeedback("Enter a number between 1-100");
+    }
+    if ((100< userInput) || (userInput < 1)){
+        insertFeedback("Enter a number between 1-100");
+    }
+    if ((!isNaN(userInput))&&((0 < userInput) && (userInput < 101))) {
+        if (guessDifference() > 50) {
+            insertFeedback("You're Ice Cold!");
+
+        } else if (guessDifference() > 40) {
+            insertFeedback("You're Cold!");
+
+        } else if (guessDifference() > 30) {
+            insertFeedback("You're warm!");
+
+        } else if (guessDifference() > 10) {
+            insertFeedback("You're Hot!");
+            
+        } else if (guessDifference() >= 5) {
+            insertFeedback("You're Very Hot!");
+
+        } else if (guessDifference()=== 0){
+            insertFeedback("Great You Got It!");
+        }
+    }
+    });
+});
